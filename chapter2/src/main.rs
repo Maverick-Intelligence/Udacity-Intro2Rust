5b1989e74cd947ebf835c5b95a098558125fb467
Nvision <nvision@maverick-intelligence.ai>
2026-05-23 13:16:06 +0200

    feat(largest-char): add specific function to find lagest char

--- chapter2/src/main.rs
+++ chapter2/src/main.rs
@@ -1,3 +1,23 @@
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+    for i in 0..list.len() {
+        if list[i] > largest {
+            largest = list[i];
+        }
+    }
+    largest
+}
+
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+    for i in 0..list.len() {
+        if list[i] > largest {
+            largest = list[i];
+        }
+    }
+    largest
+}
+
 fn main() {
     println!("Hello, world!");
 }
