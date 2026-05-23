8827ab9a09cb4260ded070c642dda50f05ac41ae
Nvision <nvision@maverick-intelligence.ai>
2026-05-23 21:07:07 +0200

    feat(main): refactor main and execute project calculator

--- chapter2/src/main.rs
+++ chapter2/src/main.rs
@@ -1,26 +1,12 @@
+mod error_handling;
 mod generics;
+mod project_calculator;
+mod traits;
 
 fn main() {
-    let list_i32 = [1234, 2345, 3456, 4567, 5678];
-    let list_char = ['a', 'b', 'c', 'd', 'e'];
-
-    let largest_i32 = generics::largest_i32(&list_i32);
-    let largest_char = generics::largest_char(&list_char);
-
-    println!("The largest i32 in 'list_32' is {}", largest_i32);
-    println!("The largest cahr in 'list_char' is {}", largest_char);
-    println!("#####################################");
-
-    let list_f32 = [0.12345, 1.23456, 2.34567, 3.45678, 4.56789];
-
-    let largest_temp_i32 = generics::largest(&list_i32);
-    let largest_temp_char = generics::largest(&list_char);
-    let largest_temp_f32 = generics::largest(&list_f32);
-
-    println!("The largest i32 in 'list_temp_i32' is {}", largest_temp_i32);
-    println!(
-        "The largest char in 'list_temp_char' is {}",
-        largest_temp_char
-    );
-    println!("The largest f32 in 'list_temp_f32' is {}", largest_temp_f32);
+    generics::execute_generics_example();
+    traits::execute_trait_example();
+    error_handling::execute_error_handling_example();
+    println!("CHAPTER 2 PROJECT: CALCULATOR");
+    project_calculator::calculator(21, 21, 84, 42, 21, 2, 84, 2, 42);
 }
